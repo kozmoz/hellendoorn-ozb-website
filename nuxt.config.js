58650a56753fdf0b6a52dddd73d317fc9a3fc8a8
kozmoz <gmw4ptnk11>
2019-11-09 22:31:11 +0100

    Facebook headers and share button added

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,13 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+
+      {propery: "og:url", content:"https://hellendoornozb.nl"},
+      {propery: "og:type", content:"website"},
+      {propery: "og:title", content:"Nee tegen 25% OZB-verhoging!"},
+      {propery: "og:description", content:"Wij als inwoners van de gemeente Hellendoorn maken een vuist tegen de gemeente door de verhoging van de OZB-belasting niet te betalen. de Automatische incasso voor de OZB-belasting zetten wij stop en wij maken een bedrag minus 25% over naar de gemeente"},
+      {propery: "og:image", content:"/fb.png"},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
