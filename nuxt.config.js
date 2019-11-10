14eeea3bc1f10b5696732ecf523e6c4cf7a2565d
kozmoz <gmw4ptnk11>
2019-11-10 17:53:03 +0100

    Fix propery name => "property"

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,11 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
 
-      {propery: "og:url", content:"https://hellendoornozb.nl"},
-      {propery: "og:type", content:"website"},
-      {propery: "og:title", content:"Nee tegen 25% OZB-verhoging!"},
-      {propery: "og:description", content:"Wij als inwoners van de gemeente Hellendoorn maken een vuist tegen de gemeente door de verhoging van de OZB-belasting niet te betalen. de Automatische incasso voor de OZB-belasting zetten wij stop en wij maken een bedrag minus 25% over naar de gemeente"},
-      {propery: "og:image", content:"https://hellendoornozb.nl/fb2.png"},
+      {property: "og:url", content:"https://hellendoornozb.nl"},
+      {property: "og:type", content:"website"},
+      {property: "og:title", content:"Nee tegen 25% OZB-verhoging!"},
+      {property: "og:description", content:"Wij als inwoners van de gemeente Hellendoorn maken een vuist tegen de gemeente door de verhoging van de OZB-belasting niet te betalen. de Automatische incasso voor de OZB-belasting zetten wij stop en wij maken een bedrag minus 25% over naar de gemeente"},
+      {property: "og:image", content:"https://hellendoornozb.nl/fb2.png"},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
