efe39e06911b240964e4c5e9a3c86d9aaa0ee18c
kozmoz <gmw4ptnk11>
2019-11-10 17:47:05 +0100

    Update Facebook image

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,17 +10,16 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Nee tegen 25% OZB-verhoging! - gemeente Hellendoorn',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
 
       {propery: "og:url", content:"https://hellendoornozb.nl"},
       {propery: "og:type", content:"website"},
       {propery: "og:title", content:"Nee tegen 25% OZB-verhoging!"},
       {propery: "og:description", content:"Wij als inwoners van de gemeente Hellendoorn maken een vuist tegen de gemeente door de verhoging van de OZB-belasting niet te betalen. de Automatische incasso voor de OZB-belasting zetten wij stop en wij maken een bedrag minus 25% over naar de gemeente"},
-      {propery: "og:image", content:"https://hellendoornozb.nl/fb.png"},
+      {propery: "og:image", content:"https://hellendoornozb.nl/fb2.png"},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
