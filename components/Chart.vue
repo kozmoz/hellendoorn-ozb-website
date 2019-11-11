8152afe0e4afdd51cf5a3348c627bab51fb525ef
kozmoz <gmw4ptnk11>
2019-11-12 00:40:38 +0100

    Fix for chart?

--- components/Chart.vue
+++ components/Chart.vue
@@ -2,21 +2,21 @@
     <div>
         <h2 class="mb-3">Vergelijk omringende gemeenten</h2>
 
+        <p class="text-center mb-0">
+            OZB-belasting omringende gemeenten in &euro;
+        </p>
         <p class="text-center">
-            OZB-belasting omringende gemeenten in €<br/>
-            <strong>Na de verhoging schiet de gemeente Hellendoorn daar opvallend ver bovenuit.</strong>
+            <strong>Na verhoging schiet de gemeente Hellendoorn daar opvallend ver bovenuit.</strong>
         </p>
 
         <div class="chart-container" id="chartContainer">
+            <!-- placeholder for canvas. -->
         </div>
-
-        <script src="/Chart.min.js"></script>
-        <script src="/drawChart.js"></script>
     </div>
 </template>
 
 <script>
-    // import "chart.js"
+    import Chart from "chart.js"
 
     // Chart.js documentation:
     // https://github.com/chartjs/Chart.js/blob/master/docs/getting-started/usage.md
@@ -27,10 +27,91 @@
             return {};
         },
         mounted() {
-            // if (process.client) {
-            //     drawChart(chartElm);
-            // }
+            if (!process.client) {
+                return;
+            }
+            setTimeout(() => {
+                const chartContainerElm = document.getElementById('chartContainer');
+                if (chartContainerElm) {
+                    const canvas = document.createElement('canvas');
+                    chartContainerElm.appendChild(canvas);
+                    drawChart(canvas);
+                } else {
+                    console.log('==== chartContainer not found!');
+                }
+            }, 1000);
         }
+    }
+
+    function drawChart(chartElm) {
+        if (!chartElm) {
+            return;
+        }
+        const proprtyPrice = 247000;
+        const ctx = chartElm.getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [
+                    'Lochem',
+                    'Rijssen-Holten',
+                    'Wierden',
+                    'Dalfsen',
+                    'Hof van Twente',
+                    'Ommen',
+                    'Raalte',
+                    'Hellendoorn',
+                ],
+                datasets: [{
+                    label: '',
+                    data: [
+                        (0.0860 / 100 * proprtyPrice).toFixed(2),
+                        (0.1019 / 100 * proprtyPrice).toFixed(2),
+                        (0.1061 / 100 * proprtyPrice).toFixed(2),
+                        (0.1102 / 100 * proprtyPrice).toFixed(2),
+                        (0.1143 / 100 * proprtyPrice).toFixed(2),
+                        (0.1199 / 100 * proprtyPrice).toFixed(2),
+                        (0.1352 / 100 * proprtyPrice).toFixed(2),
+                        (0.17925 / 100 * proprtyPrice).toFixed(2)
+                    ],
+                    backgroundColor: [
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(64, 64, 64, 0.2)',
+                        'rgba(255, 0, 0, 0.8)',
+                    ],
+                    borderColor: [
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(64, 64, 64, 1)',
+                        'rgba(255, 0, 0, 1)',
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                // https://github.com/chartjs/Chart.js/blob/master/docs/configuration/legend.md
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+                },
+                maintainAspectRatio: false
+            }
+        });
     }
 
 </script>
