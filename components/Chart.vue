260a9931103af7591efce61db46adc463b7aa72b
kozmoz <gmw4ptnk11>
2019-11-11 23:32:28 +0100

    Fix responsive chart styling

--- components/Chart.vue
+++ components/Chart.vue
@@ -2,14 +2,12 @@
     <div>
         <h2 class="mb-3">Vergelijk omringende gemeenten</h2>
 
-        <div class="chart-wrapper" style="margin: 0 auto">
-            <p class="text-center">
-                OZB-belasting omringende gemeenten in €<br/>
-                <strong>Na de verhoging schiet de gemeente Hellendoorn daar opvallend ver bovenuit.</strong>
-            </p>
-            <client-only>
-                <canvas id="ozbChart" width="400" :height="height"></canvas>
-            </client-only>
+        <p class="text-center">
+            OZB-belasting omringende gemeenten in €<br/>
+            <strong>Na de verhoging schiet de gemeente Hellendoorn daar opvallend ver bovenuit.</strong>
+        </p>
+        <div class="chart-container">
+            <canvas id="ozbChart"></canvas>
         </div>
     </div>
 </template>
@@ -25,30 +23,16 @@
         data: function () {
             return {};
         },
-        computed: {
-            height() {
-                if (process.client) {
-                    return global.window.innerWidth > 576 ? 200 : 400;
-                }
-                return 0;
-            }
-        },
         mounted() {
-            if (!process.client) {
-                return;
-            }
-
-            // It takes two ticks to render the canvas.
-            this.$nextTick(() => {
-                this.$nextTick(() => {
-                    const chartElm = global.document.getElementById('ozbChart');
-                    drawChart(chartElm);
-                });
-            });
+            const chartElm = global.document.getElementById('ozbChart');
+            drawChart(chartElm);
         }
     }
 
     function drawChart(chartElm) {
+        if (!chartElm) {
+            return;
+        }
         const proprtyPrice = 247000;
         const ctx = chartElm.getContext('2d');
         const ozbChart = new Chart(ctx, {
@@ -107,23 +91,33 @@
                 scales: {
                     yAxes: [{
                         ticks: {
-                            beginAtZero: true
+                            beginAtZero: false
                         }
                     }]
-                }
+                },
+                maintainAspectRatio: false
             }
         });
     }
 </script>
 
 <style scoped>
-    .chart-wrapper {
+    /* https://www.chartjs.org/docs/latest/general/responsive.html */
+    .chart-container {
+        position: relative;
+        height: 300px;
+        max-width: 90vw;
     }
 
+    @media (max-width: 767.98px) {
+        .chart-container {
+            width: 80vw
+        }
+    }
     @media (max-width: 575.98px) {
-        .chart-wrapper {
-            max-width: 90vw;
-            width: 90vw;
+        .chart-container {
+            height: 400px;
+            width: 90vw
         }
     }
 </style>
