ada5487b4876bcbfede7ff7d53dfb065d04deac8
kozmoz <gmw4ptnk11>
2019-11-12 14:57:09 +0100

    Added FAQ about how to complain.

--- components/FaqModule.vue
+++ components/FaqModule.vue
@@ -91,15 +91,16 @@
             </p>
         </div>
 
+
         <p>
-            <strong>Wanneer ontvang ik mijn aanslag voor de OZB-belasting?</strong>
+            <strong>Wanneer ontvang ik mijn aanslag voor OZB-belasting?</strong>
             <a v-if="!faq_ozb_aanslag" @click.prevent="faq_ozb_aanslag=true" href="#">Klik hier voor het antwoord</a>
             <a v-if="faq_ozb_aanslag" @click.prevent="faq_ozb_aanslag=false" href="#">Verberg het antwoord</a>
         </p>
         <div class="pl-3 pb-3" v-if="faq_ozb_aanslag">
             <p>
                 Afgelopen jaar werd de aanslag voor de gemeentelijke belastingen 25 februari verstuurd naar alle
-                inwoners van de gemeente Hellendoorn. <br />
+                inwoners van de gemeente Hellendoorn. <br/>
                 <em><u>De aanslag voor 2020 wordt eind februari verwacht</u></em>.
             </p>
             <p>
@@ -112,10 +113,83 @@
             <p>
                 Naast de onroerende-zaakbelastingen (OZB) bevat deze aanslag ook de afvalstoffenheffing voor de
                 inzameling van afvalstoffen, de rioolheffing en de hondenbelasting. De aanslag kan in tien termijnen
-                via automatisch-incasso of in één keer via een bankoverschrijving betaald worden.
+                via automatisch-incasso of in één-, twee- of vier keer via een bankoverschrijving betaald worden.
             </p>
         </div>
 
+
+        <p>
+            <strong>Klagen over hoogte van OZB-belasting?</strong>
+            <a v-if="!faq_ozb_klacht" @click.prevent="faq_ozb_klacht=true" href="#">Klik hier voor het
+                antwoord</a>
+            <a v-if="faq_ozb_klacht" @click.prevent="faq_ozb_klacht=false" href="#">Verberg het antwoord</a>
+        </p>
+        <div class="pl-3 pb-3" v-if="faq_ozb_klacht">
+            <p>
+                De gemeenteraad heeft beslist dat er een verhoging komt van de OZB-belasting.
+                <strong>Aan- en of opmerkingen of een klacht over de hoogte van de OZB-belasting?
+                    Laat het weten aan de gemeente!</strong>
+            </p>
+            <p>
+                De gemeente Hellendoorn is op de volgende manieren te bereiken:
+            </p>
+            <ul>
+                <li>
+                    <em><u>E-mail</u></em> - bereikbaar via <a class="normal-link"
+                                                        href="mailto:gemeente@hellendoorn.nl?subject=OZB-belasting">gemeente@hellendoorn.nl</a>.
+                </li>
+                <li>
+                    <em><u>Telefoon</u></em> - Voor informatie over aanslagen, WOZ-waarderingen, bezwaar, betalingen,
+                    betalingsregelingen
+                    en kwijtschelding belt u met 0548-630216.
+                </li>
+                <li>
+                    <em><u>Post</u></em> - Brieven over de gemeentelijke belastingen stuurt u naar de heffingsambtenaar,
+                    postbus 200, 7440 AE Nijverdal. <br/>
+                    Vermeld in uw brief het aanslagnummer, belastingjaar, soort belasting en ook uw telefoonnummer en
+                    eventueel emailadres.
+                </li>
+                <li>
+                    <em><u>Bezoek</u></em> - U kunt langskomen in het Huis voor Cultuur en Bestuur aan de
+                    Willem-Alexanderstraat 7 te Nijverdal. Kijk voor actuele openingstijden op <a
+                        href="https://hellendoorn.nl" target="_blank" rel="noopener" class="normal-link">www.hellendoorn.nl</a>
+                    of op de gemeentelijke pagina’s in het Twents Volksblad.
+                </li>
+            </ul>
+        </div>
+
+        <!--
+        <p>
+            <strong>Wanneer ontvang ik mijn aanslag voor de OZB-belasting?</strong>
+            <a v-if="!faq_ozb_aanslag2" @click.prevent="faq_ozb_aanslag2=true" href="#">Klik hier voor het
+                antwoord</a>
+            <a v-if="faq_ozb_aanslag2" @click.prevent="faq_ozb_aanslag2=false" href="#">Verberg het antwoord</a>
+        </p>
+        <div class="pl-3 pb-3" v-if="faq_ozb_aanslag2">
+            <p>
+                Als u het voorgaande jaar betaalde via automatische incasso, heeft de gemeente een machtiging
+                om het bedrag automatisch af te schrijven. Het incassant ID is NL98ZZZ082218590000.
+            </p>
+            <p>
+                Bij automatische incasso schrijft de gemeente de aanslag in tien maandelijkse termijnen
+                automatisch van de rekening af.
+
+                • Internetbankieren of overschrijfformulier. Afhankelijk van het aanslagbedrag moet u de aanslag in
+                twee of vier termijnen
+                betalen. U MOET HET EXACTE AANSLAGNUMMER VERMELDEN, ANDERS WORDT UW BETALING NIET OF ONJUIST
+                VERWERKT.
+                Het bankrekeningnummer is NL54 BNGH 0285 0667 65.
+
+                WANNEER MOET IK BETALEN?
+                De betalingstermijnen staan op de voorkant van dit aanslagbiljet. Bij vier betalingstermijnen moet u
+                bij elke betalingstermijn een vierde deel van het totaal verschuldigde bedrag betalen.
+                Bij twee betalingstermijnen moet u bij elke betalingstermijn de helft betalen. Uiteraard mag u voor
+                of op de eerste betalingstermijn het gehele bedrag ineens betalen. Wanneer er tien
+                betalingstermijnen op uw aanslag staan, betekent dit dat de gemeente automatisch gaat afschrijven.
+                Het is niet toegestaan om zelf in tien keer over te maken.
+            </p>
+        </div>
+        -->
 
     </div>
 </template>
@@ -129,7 +203,9 @@
                 faq_wiezijnwij: false,
                 faq_ozb_hellendoorn: false,
                 faq_ozb_hellendoorn2: false,
-                faq_ozb_aanslag: false
+                faq_ozb_aanslag: false,
+                faq_ozb_aanslag2: false,
+                faq_ozb_klacht: false
             };
         }
     }
